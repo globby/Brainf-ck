750ac1ada8b253c97392ed441cc7925b3d685e51
Nate <catalyst@catalyst.pw>
2014-09-04 21:41:07 -0700

    Add bf.py

---
+++ bf.py
@@ -0,0 +1,74 @@
+import sys
+class Tape(object):
+	def __init__(self):
+		self.tape = [0 for i in range(2000)]
+		self.point = 0
+	def inc_p(self):
+		self.point += 1
+		if not self.point < len(self.tape):
+			print "Error: Tape index > %s" % self.point
+			sys.exit(-1)
+	def dec_p(self):
+		self.point -= 1
+		if self.point < 0:
+			print "Error: Tape index < 0"
+			sys.exit(-1)
+	def inc(self):
+		self.tape[self.point] += 1
+	def dec(self):
+		self.tape[self.point] -= 1
+	def get(self):
+		return self.tape[self.point]
+	def set(self, val):
+		self.tape[self.point] = val
+def getbrackets(code):
+	brackets = {}
+	opening = []
+	for i, v in enumerate(code):
+		if v == "[":
+			opening.append(i)
+		elif v == "]":
+			if opening:
+				brackets[i] = opening.pop()
+			else:
+				print "Error: Unbalanced brackets"
+				sys.exit(-1)
+	return brackets
+
+
+def run(code):
+	b = getbrackets(code)
+	tape = Tape()
+	p = 0
+	while p < len(code):
+		c = code[p]
+		if c == "+":
+			tape.inc()
+		elif c == "-":
+			tape.dec()
+		elif c == ">":
+			tape.inc_p()
+		elif c == "<":
+			tape.dec_p()
+		elif c == "]":
+			if tape.get():
+				p = b[p]
+		elif c == ".":
+			sys.stdout.write(chr(tape.get()%255))
+		elif c == ",":
+			r = raw_input("")
+			if r:
+				tape.set(ord(r[0]))
+			else:
+				p = len(code)
+		p += 1
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		run(raw_input(""))
+	else:
+		try:
+			with open(sys.argv[1]) as f:
+				run(f.read())
+		except:
+			print "Error: Couldn't open file"
